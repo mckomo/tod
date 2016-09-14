5e8ffc54cb67676b6ed8d2e72fe0220581007506
mckomo <mckomo@gmail.com>
2016-09-14 09:54:48 +0200

    Travis class

--- lib/tod/travis.rb
+++ lib/tod/travis.rb
@@ -1,8 +1,29 @@
+require 'yaml'
+require 'active_support/hash_with_indifferent_access'
+
 module Tod
 
   class Travis
 
+    def initialize(yml)
+      @yml = yml.symbolize_keys
+    end
+
+    def env
+      Hash(@yml[:env]) || {}
+    end
+
+    def script
+      Array(@yml[:script]) || []
+    end
+
+    def before
+      Array(@yml[:before_script]) || []
+    end
+
+    def install
+      Array(@yml[:before_install]) || []
+    end
+
   end
-
-
 end
---
+++ spec/travis_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe Tod::Travis do
+
+  subject { Tod::Travis.new yml }
+
+  describe '.env' do
+
+    let(:yml) { Hash('env' => { 'DOCKER_COMPOSE_VERSION' => 1.8 }) }
+
+    it 'returns dictionary of environmental variables' do
+      expect(subject.env).to eq({ 'DOCKER_COMPOSE_VERSION' => 1.8 })
+    end
+
+  end
+
+  describe '.script' do
+
+    let(:yml) { Hash('script' => 'rspec') }
+
+    it 'returns array of test scripts' do
+      expect(subject.script).to eq(['rspec'])
+    end
+
+  end
+
+  describe '.before' do
+
+    let(:yml) { Hash('before_script' => ['rails db:migrate', 'rails serve'],  ) }
+
+    it 'returns array of before test scripts' do
+      expect(subject.before).to eq(['rails db:migrate', 'rails serve'])
+    end
+
+  end
+
+  describe '.install' do
+
+    let(:yml) { Hash('before_install' => ['apt-get update', 'apt-get install git']) }
+
+    it 'returns array of setup scripts' do
+      expect(subject.install).to eq(['apt-get update', 'apt-get install git'])
+    end
+
+  end
+
+end
